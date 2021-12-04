bba5a286d5e81a17f0d60bad053251c774166162
Alexandros Stergiou <jutaroroche@gmail.com>
2021-12-04 17:32:00 +0100

    Fix de la taille de la fenetre

--- src/main/java/com/example/jolicitefx/HelloApplication.java
+++ src/main/java/com/example/jolicitefx/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1520, 840);
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 820);
         stage.setTitle("Programmation Culturelle");
         stage.setScene(scene);
         stage.show();
